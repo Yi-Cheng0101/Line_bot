a153df76e7b2218c38ae87fdbeb875079931c39e
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-03-12 15:45:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -37,11 +37,5 @@ def handle_message(event):
     get_message = event.message.text
 
     # Send To Line
-    #reply = TextSendMessage(text=f"{get_message}")
-    #line_bot_api.reply_message(event.reply_token, url="http://web.cs.nthu.edu.tw/p/406-1174-193149,r109.php")
-    message = ImageSendMessage(
-    original_content_url='http://web.cs.nthu.edu.tw/var/file/174/1174/pictures/905/m/mczh-tw1280x800_large36424_319472822358.png',
-    preview_image_url='http://web.cs.nthu.edu.tw/var/file/174/1174/pictures/905/m/mczh-tw1280x800_large36424_319472822358.png'
-    )
-    line_bot_api.reply_message(event.reply_token, message)
-    #line_bot_api.reply_message(event.reply_token, reply)
+    reply = TextSendMessage(text=f"{get_message}")
+    line_bot_api.reply_message(event.reply_token, reply)
