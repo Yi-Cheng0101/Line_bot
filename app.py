97835cd84b2b65d2d24758ce5a11232b0ff29125
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-03-12 19:04:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -46,6 +46,6 @@ def handle_message(event):
         line_bot_api.reply_message(event.reply_token, reply)
         return 0
     
- if __name__ == '__main__':
+if __name__ == '__main__':
     app.run()
 
