8ef0cfab6323618e0d21ca344be9ad75f0072733
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-03-12 18:56:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,26 +31,6 @@ def callback():
         return "OK"
 
 
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    get_message = event.message.text
-
-    # Send To Line
-    tt = 'https://p.facebook.com/csofficeNTHU/photos/a.1864273603844281/2782546688683630/?type=3&source=48&__tn__=EH-R'
-    reply = TextSendMessage(text=tt)
-    line_bot_api.reply_message(event.reply_token, reply)
-    
-    
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    get_message = event.message.text
-
-    # Send To Line
-    tt = 'https://p.facebook.com/csofficeNTHU/photos/a.1864273603844281/2782546688683630/?type=3&source=48&__tn__=EH-R'
-    reply = TextSendMessage(text=tt)
-    line_bot_api.reply_message(event.reply_token, reply)
-    
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
