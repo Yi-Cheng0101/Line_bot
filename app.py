5e63f60f4fe750c7dd0b736a15fc11ca117dcc39
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-03-12 10:45:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,10 +10,9 @@ from linebot.models import MessageEvent, TextMessage, TextSendMessage
 
 app = Flask(__name__)
 
-#line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
-#handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
-line_bot_api = LineBotApi(os.environ.get("9cXJht4q7WSduoh1eLBpkh+KKfVGYQDxJO0M73yQsyYHZuwECrn57YEEiV3N/TRxxqxI/uPVjNMAl9ByoQmvrZgVkX3c7cBvh+4OJM8t+y0OHY2YPX7i7eBkBFeXoQjLSl0vl46aquYGXvy41h29PQdB04t89/1O/w1cDnyilFU="))
-handler = WebhookHandler(os.environ.get("4818ef2864e2fefac2de9b5de6d1eb95"))
+line_bot_api = LineBotApi(os.environ.get("CHANNEL_ACCESS_TOKEN"))
+handler = WebhookHandler(os.environ.get("CHANNEL_SECRET"))
+
 
 @app.route("/", methods=["GET", "POST"])
 def callback():
@@ -32,7 +31,6 @@ def callback():
         return "OK"
 
 
-
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     get_message = event.message.text
@@ -40,7 +38,3 @@ def handle_message(event):
     # Send To Line
     reply = TextSendMessage(text=f"{get_message}")
     line_bot_api.reply_message(event.reply_token, reply)
-    
-    
-
-   
