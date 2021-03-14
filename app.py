ccfe117a73dde21fdf7463ac66232798a194948a
Yi-Cheng Hsiao <46053065+Yi-Cheng0101@users.noreply.github.com>
2021-03-15 03:57:41 +0800

    Update app.py

--- app.py
+++ app.py
@@ -258,6 +258,26 @@ def handle_message(event):
                                 uri='http://example.com/2'
                             )
                         ]
+                    ),
+                    CarouselColumn(
+                        thumbnail_image_url='https://example.com/item2.jpg',
+                        title='this is menu2',
+                        text='description2',
+                        actions=[
+                            PostbackTemplateAction(
+                                label='postback2',
+                                text='postback text2',
+                                data='action=buy&itemid=2'
+                            ),
+                            MessageTemplateAction(
+                                label='message2',
+                                text='message text2'
+                            ),
+                            URITemplateAction(
+                                label='uri2',
+                                uri='http://example.com/2'
+                            )
+                        ]
                     )
                 ]
             )
